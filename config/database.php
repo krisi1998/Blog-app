ed688ed22ff39afaabaec881414c709a26d46349
krisi1998 <Krisi.Gjika@fti.edu.al>
2020-12-25 20:24:24 +0100

    Update database connection

--- config/database.php
+++ config/database.php
@@ -1,3 +1,4 @@
+$DATABASE_URL=parse_url(‘postgres://kqqcmjspsknksu:82cc17b5d2db32fed7b977e9d1f33cb7a91608ae3c992ee75a1aca3351b6ac00@ec2-52-71-153-228.compute-1.amazonaws.com:5432/dae7t68tubpfu2’);
 <?php
 
 use Illuminate\Support\Str;
@@ -15,7 +16,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -65,12 +66,11 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"],"/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
